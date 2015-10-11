d7ad8053f8b1f5f78dbd20ea78c9d092600b5f3b
Mathias Nagler <mathias.nagler@me.com>
2015-10-11 21:28:37 +0200

    Adds iOS Example

---
+++ iOS Example/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  iOS Example
+//
+//  Created by Mathias Nagler on 08.09.15.
+//  Copyright © 2015 Mathias Nagler. All rights reserved.
+//
+
+import UIKit
+import SwiftyAcknowledgements
+
+class ViewController: UIViewController {
+
+    @IBAction func showAcknowledgements() {
+        let vc = AcknowledgementsTableViewController()
+        vc.headerText = "SwiftyAcknowledgements makes use of the following third party libraries:"
+        vc.footerText = "Third party libraries integrated using Carthage:\nhttp://github.com/carthage"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+}
+
