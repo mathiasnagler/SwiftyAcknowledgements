21618f48e744e5f183bd8956960648dd30c3479c
Mathias Nagler <mathias.nagler@me.com>
2020-07-09 13:29:54 +0200

    Updates project to Swift 5

--- SwiftyAcknowledgements/UIFontDescriptorExtensions.swift
+++ SwiftyAcknowledgements/UIFontDescriptorExtensions.swift
@@ -12,8 +12,8 @@ internal extension UIFontDescriptor {
     
     /// Returns the point-size of the preffered font for a text style
     /// The user can influence this value by changing the font-size setting
-    internal class func preferredFontSize(withTextStyle style: String) -> CGFloat {
-        let style = self.preferredFontDescriptor(withTextStyle: UIFontTextStyle(rawValue: style))
+    class func preferredFontSize(withTextStyle style: String) -> CGFloat {
+        let style = self.preferredFontDescriptor(withTextStyle: UIFont.TextStyle(rawValue: style))
         return style.pointSize
     }
 
