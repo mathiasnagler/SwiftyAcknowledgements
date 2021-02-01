4f372f65b1191919cd8c2ac3d2dd611a62e05958
Michael Redig <mredig@gmail.com>
2021-01-31 20:15:10 -0600

    (nit) code organization

--- Sources/GenerateLicenseFile/main.swift
+++ Sources/GenerateLicenseFile/main.swift
@@ -2,40 +2,9 @@
 
 import Foundation
 
-// MARK: String Extensions
-
-extension String {
-    func ends(with str: String) -> Bool {
-        let lc = self.lowercased()
-        let lcStr = str.lowercased()
-        if let range = lc.range(of: lcStr, options: .backwards) {
-            return range.upperBound == lc.endIndex
-        }
-        return false
-    }
-}
-
-extension IteratorProtocol {
-    func forEach(_ body: (Element) -> ()) {
-        var copy = self
-        while let current = copy.next() {
-            body(current)
-        }
-    }
-
-    func map<Output>(_ body: (Element) -> Output) -> [Output] {
-        var copy = self
-        var result = [Output]()
-        while let current = copy.next() {
-            result.append(body(current))
-        }
-        return result
-    }
-}
-
-// MARK: Internal functions
 let fm = FileManager.default
 
+// MARK: Internal functions
 func locateLicense(inProject projectFolder: URL) -> URL? {
     var isDir: ObjCBool = false
     fm.fileExists(atPath: projectFolder.path, isDirectory: &isDir)
@@ -137,3 +106,27 @@ func main() {
 
 
 main()
+
+
+// MARK: Convenience Extensions
+extension String {
+    func ends(with str: String) -> Bool {
+        let lc = self.lowercased()
+        let lcStr = str.lowercased()
+        if let range = lc.range(of: lcStr, options: .backwards) {
+            return range.upperBound == lc.endIndex
+        }
+        return false
+    }
+}
+
+extension IteratorProtocol {
+    func map<Output>(_ body: (Element) -> Output) -> [Output] {
+        var copy = self
+        var result = [Output]()
+        while let current = copy.next() {
+            result.append(body(current))
+        }
+        return result
+    }
+}
