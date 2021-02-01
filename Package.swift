c2fa19920237bc1a821039cb54f3d8761d0dda7a
Michael Redig <mredig@gmail.com>
2021-01-31 21:14:29 -0600

    (fix) added tvos 9 min requirement to package to match readme

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftyAcknowledgements",
 	platforms: [
-		.iOS(.v8)
+		.iOS(.v8),
+		.tvOS(.v9),
 	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
